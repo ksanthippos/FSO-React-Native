7647f84eda6658e10472f420c280efb532c258f0
mikaelrauhala <mikael.rauhala@eduespoo.fi>
2020-11-06 15:20:20 +0200

    ex10.8 done

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import { Button, TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
 import { Formik } from 'formik'
-import Text from './Text';
 import FormikTextInput from './FormikTextInput'
 
 const styles = StyleSheet.create({
-  heading: {
-    fontSize: 30,
+  flexContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 10,
+  },
+  input: {
+    borderColor: '#999b9b',
+    borderWidth: 1.5,
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#4b9ed4',
+    borderColor: '#3887bd',
+    borderWidth: 0,
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  text: {
+    color: '#ffffff',
+    textAlign: 'center',
   }
+  
 })
 
 const initialValues = {
@@ -16,13 +42,24 @@ const initialValues = {
 }
 
 const SignInForm = ({ onsubmit }) => {
+
   return (
-    <View>
-      <Text style={styles.heading}>Login</Text>
-      <FormikTextInput name="username" placeholder="Weight (kg)" />
-      <FormikTextInput secureTextEntry name="password" placeholder="Height (m)" />
+    <View style={styles.flexContainer}>
+      <FormikTextInput 
+        style={styles.input} 
+        name="username" 
+        placeholder="Username:" 
+      />
+      <FormikTextInput 
+        style={styles.input} 
+        secureTextEntry 
+        name="password" 
+        placeholder="Password:" 
+      />
       <TouchableWithoutFeedback>
-        <Button title={`Login`} onPress={onsubmit} />
+        <View style={styles.button}>
+          <Text style={styles.text} onPress={onsubmit}>Login</Text>
+        </View>
       </TouchableWithoutFeedback>
     </View>
   );
@@ -31,7 +68,7 @@ const SignInForm = ({ onsubmit }) => {
 const SignIn = () => {
   const onSubmit = (values) => {
     console.log('username: ', values.username)
-    console.log('password: ', values.password);
+    console.log('password: ', values.password)
   }
 
   return(
