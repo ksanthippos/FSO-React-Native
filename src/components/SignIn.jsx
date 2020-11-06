7630abeb307e6fde845efdf68fc87bff4d50d40a
mikaelrauhala <mikael.rauhala@eduespoo.fi>
2020-11-06 13:54:26 +0200

    simple formik form working

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import { Button, TouchableWithoutFeedback, View } from 'react-native';
+import { Button, TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
+import { Formik } from 'formik'
 import Text from './Text';
-import TextInput from './TextInput'
 import FormikTextInput from './FormikTextInput'
 
-const SignIn = () => {
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 30,
+  }
+})
+
+const initialValues = {
+  username: '',
+  password: '',
+}
+
+const SignInForm = ({ onsubmit }) => {
   return (
     <View>
-      <Text>Login</Text>
-      <TextInput 
-        placeholder="Username"
-      />
-      <TextInput 
-        placeholder="Password"
-      />
+      <Text style={styles.heading}>Login</Text>
+      <FormikTextInput name="username" placeholder="Weight (kg)" />
+      <FormikTextInput secureTextEntry name="password" placeholder="Height (m)" />
       <TouchableWithoutFeedback>
-        <Button title={`Login`} />
+        <Button title={`Login`} onPress={onsubmit} />
       </TouchableWithoutFeedback>
     </View>
   );
+} 
+
+const SignIn = () => {
+  const onSubmit = (values) => {
+    console.log('username: ', values.username)
+    console.log('password: ', values.password);
+  }
+
+  return(
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onsubmit={handleSubmit} /> }
+    </Formik>
+  )
 };
 
 export default SignIn;
